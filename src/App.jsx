bfe54d97c9311db348f226f1143a73e407dc4200
alexeyloon <alexeyloon921@gmail.com>
2022-02-14 14:27:18 +0300

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
+import News from './pages/News';
 
 function App() {
   const [data, setData] = useState({});
-  const url = "https://62061fb7161670001741bf36.mockapi.io/api/news?page=1&limit=10";
+  const url = 'https://62061fb7161670001741bf36.mockapi.io/api/news?page=1&limit=10';
   const fetchData = async () => {
     try {
+      // eslint-disable-next-line no-undef
       const response = await fetch(url, {
-        "method": "GET",
+        method: 'GET',
       });
       const result = await response.json();
       console.log(result);
       setData(result);
     } catch (error) {
-      console.log("error", error);
+      console.log('error', error);
     }
   };
   useEffect(() => {
-    console.log('tick')
-
+    console.log('tick', data, 5, 6);
 
     fetchData();
   }, []);
+
+  return (
+    <News />
+  );
 }
-
-
 
 export default App;
---
+++ src/pages/News/News.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function News() {
+  return (<div>New list</div>);
+}
+
+export default News;
