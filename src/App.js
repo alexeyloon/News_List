fee638225160fabea1f8d8666f6a7b1ba6ca54a3
alexeyloon <alexeyloon921@gmail.com>
2022-02-14 13:06:07 +0300

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState({});
+  const url = "https://62061fb7161670001741bf36.mockapi.io/api/news?page=1&limit=10";
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url, {
+        "method": "GET",
+      });
+      const result = await response.json();
+      console.log(result);
+      setData(result);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  useEffect(() => {
+    console.log('tick')
+
+
+    fetchData();
+  }, []);
 }
+
+
 
 export default App;
