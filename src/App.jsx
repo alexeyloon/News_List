bdc7b5b405605177f75df189e10e65970bc2ecd4
alexeyloon <alexeyloon921@gmail.com>
2022-02-15 15:06:21 +0300

    add news

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,22 @@ import React, { useEffect, useState } from 'react';
 import News from './pages/News';
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
   const [data, setData] = useState({});
   const url = 'https://62061fb7161670001741bf36.mockapi.io/api/news?page=1&limit=10';
-  const fetchData = async () => {
-    try {
-      // eslint-disable-next-line no-undef
-      const response = await fetch(url, {
-        method: 'GET',
-      });
-      const result = await response.json();
-      console.log(result);
-      setData(result);
-    } catch (error) {
-      console.log('error', error);
-    }
+  const axiosData = async () => {
+    // eslint-disable-next-line no-undef
+    const response = axios.get(url)
+      .then((result) => console.log('success:', result))
+      .catch((error) => console.log('error:', error));
+
+    console.log('error', response);
   };
+
   useEffect(() => {
     console.log('tick', data, 5, 6);
 
-    fetchData();
+    axiosData();
   }, []);
 
   return (
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
 
 function News() {
-  return (<div>New list</div>);
+  return (
+    <>
+      <div>
+        <span>1</span>
+        <span> 2</span>
+        <span> 3</span>
+        <span> 4</span>
+        <span> 5</span>
+        <span> ...</span>
+      </div>
+      <h1> News list </h1>
+      <div>News 1</div>
+      <div>
+        <div>
+          <textarea />
+        </div>
+        <div>
+          <button type="button">Add News</button>
+        </div>
+      </div>
+      <div>News 2</div>
+      <div>
+        <div>
+          <textarea />
+        </div>
+        <div>
+          <button type="button">Add News</button>
+        </div>
+      </div>
+      <div>News 3</div>
+      <div>
+        <div>
+          <textarea />
+        </div>
+        <div>
+          <button type="button">Add News</button>
+        </div>
+      </div>
+
+    </>
+  );
 }
 
 export default News;
